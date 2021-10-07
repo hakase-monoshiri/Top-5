b841b68bd7e215172cc9265b27600f04bc53f0ea
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-10-07 19:54:50 -0400

    generate rails scaffols for user, list, category models

---
+++ top-5-api/db/migrate/20211007234922_create_lists.rb
@@ -0,0 +1,17 @@
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lists do |t|
+      t.integer :user_id
+      t.integer :category_id
+      t.string :name
+      t.text :description
+      t.text :one
+      t.text :two
+      t.text :three
+      t.text :four
+      t.text :five
+
+      t.timestamps
+    end
+  end
+end
